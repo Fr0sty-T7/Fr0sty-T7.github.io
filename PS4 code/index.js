a7864e785bb8033a305044174a4ecac144e09f6c
Fr0sty-T7 <149890939+Fr0sty-T7@users.noreply.github.com>
2023-11-28 16:21:59 +0300

    Add files via upload

---
+++ PS4 code/index.js
@@ -0,0 +1,76 @@
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+  alert("Sorry can't inspect this page");
+});
+document.addEventListener("keydown", function (e) {
+  if (e.key === "F12" || e.keyCode === 123) {
+    alert("Sorry can't inspect this page");
+    e.preventDefault();
+  }
+});
+
+let controllerIndex = null;
+
+window.addEventListener("gamepadconnected", (event) => {
+  const gamepad = event.gamepad;
+  controllerIndex = gamepad.index;
+  console.log("connected");
+});
+window.addEventListener("gamepaddisconnected", (event) => {
+  controllerIndex = null;
+  console.log("disconnected");
+});
+function handleButtons(buttons) {
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    const buttonElement = document.getElementById(`controller-b${i}`);
+    const selectedButtonClass = "pressed";
+    let trrigerLeft = document.querySelector(".triggers .left");
+    let trrigerRight = document.querySelector(".triggers .right");
+
+    if (buttonElement) {
+      if (button.value > 0) {
+        buttonElement.classList.add(selectedButtonClass);
+        if (buttons[6].value > 0) {
+          trrigerLeft.style.opacity = `${button.value * 100}%`;
+        }
+        if (buttons[7].value > 0) {
+          trrigerRight.style.opacity = `${button.value * 100}%`;
+        }
+      } else {
+        buttonElement.classList.remove(selectedButtonClass);
+        if (buttons[6].value <= 0) {
+          trrigerLeft.style.opacity = `0%`;
+        }
+        if (buttons[7].value <= 0) {
+          trrigerRight.style.opacity = `0%`;
+        }
+      }
+    }
+  }
+}
+function updateStick(elementId, leftRightAxis, upDownAxis) {
+  const multiplier = 25;
+  const stickLeftRight = leftRightAxis * multiplier;
+  const stickUpDown = upDownAxis * multiplier;
+  const stick = document.getElementById(elementId);
+  const x = Number(stick.dataset.originalXPosition);
+  const y = Number(stick.dataset.originalYPosition);
+  stick.style.transform =
+    "rotateX(" + stickUpDown * -1 + "deg) rotateY(" + stickLeftRight + "deg)";
+  stick.style.top = `${y + stickUpDown}px`;
+  stick.style.left = `${x + stickLeftRight}px`;
+}
+function handleSticks(axes) {
+  updateStick("controller-b10", axes[0], axes[1]);
+  updateStick("controller-b11", axes[2], axes[3]);
+}
+function gameLoop() {
+  if (controllerIndex !== null) {
+    const gamepad = navigator.getGamepads()[controllerIndex];
+    handleButtons(gamepad.buttons);
+    handleSticks(gamepad.axes);
+  }
+  requestAnimationFrame(gameLoop);
+}
+gameLoop();
